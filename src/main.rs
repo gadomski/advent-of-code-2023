f53ca3a4f02a5d76f5da40a877980a5d9ce6d331
Pete Gadomski <pete.gadomski@gmail.com>
2023-12-02 09:50:07 -0700

    refactor: change printing

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use anyhow::Result;
 use aoc::{day_01, day_02};
 
 fn main() -> Result<()> {
-    println!("Day 01, part 1: {}", day_01::part_1()?);
-    println!("Day 01, part 2: {}", day_01::part_2()?);
-    println!("Day 02, part 1: {}", day_02::part_1()?);
-    println!("Day 02, part 2: {}", day_02::part_2()?);
+    println!("Day 01 part 1: {}", day_01::part_1()?);
+    println!("       part 2: {}", day_01::part_2()?);
+    println!("Day 02 part 1: {}", day_02::part_1()?);
+    println!("       part 2: {}", day_02::part_2()?);
     Ok(())
 }
